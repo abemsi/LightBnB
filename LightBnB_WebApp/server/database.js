9dddc454bb0bfd6f6c496388c741eac4dea3a867
Amy Bemister <amy.bemister@gmail.com>
2019-11-14 20:26:04 +0000

    Update functions to use the lightbnb database with SQL queries

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -17,18 +17,30 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+// const getUserWithEmail = function(email) {
+//   let user;
+//   for (const userId in users) {
+//     user = users[userId];
+//     if (user.email.toLowerCase() === email.toLowerCase()) {
+//       break;
+//     } else {
+//       user = null;
+//     }
+//   }
+//   return Promise.resolve(user);
+// }
+
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`
+  SELECT *
+  FROM users
+  WHERE email = $1;
+  `,[email])
+  .then(res => {
+    return res.rows[0] || null;
+  })
 }
+
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -36,9 +48,21 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+// const getUserWithId = function(id) {
+//   return Promise.resolve(users[id]);
+// }
+
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+  SELECT *
+  FROM users
+  WHERE id = $1;
+  `, [id])
+  .then(res => {
+    return res.rows[0] || null;
+  })
 }
+
 exports.getUserWithId = getUserWithId;
 
 
@@ -47,12 +71,24 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
+// const addUser =  function(user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// }
+
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `, [user.name, user.email, user.password])
+  .then(res => {
+    return res.rows;
+  })
 }
+
 exports.addUser = addUser;
 
 /// Reservations
@@ -82,15 +118,15 @@ exports.getAllReservations = getAllReservations;
 //   }
 //   return Promise.resolve(limitedProperties);
 // }
-// exports.getAllProperties = getAllProperties;
 
 const getAllProperties = function(options, limit = 10) {
-  pool.query(`
+  return pool.query(`
   SELECT * FROM properties
   LIMIT $1
   `, [limit])
   .then(res => res.rows);
 }
+exports.getAllProperties = getAllProperties;
 
 
 /**
